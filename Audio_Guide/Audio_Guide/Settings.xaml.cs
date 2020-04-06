c3aeea48f0600ffcd359e5f8bae54061b02d3a10
Princess-10 <44162753+Princess-10@users.noreply.github.com>
2020-04-06 17:02:09 +0100

    Added Settings Page

---
+++ Audio_Guide/Audio_Guide/Settings.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Audio_Guide
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Settings : ContentPage
+    {
+        public Settings()
+        {
+            InitializeComponent();
+            myPicker.SelectedItem = "6";
+            Categories.SelectedItem = "General";
+            dist.Text = "5";
+            Distance.Value = 5;
+            Star_Rating.SelectedItem = "0+";
+        }
+        private void Slide_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+
+            dist.Text = Distance.Value.ToString();
+        }
+    }
+}
